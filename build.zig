ee3803b95f77d31f9701518e3d2b6cfaae2e193c
Henri Vasserman <henv@hot.ee>
2022-06-02 12:24:18 +0300

    add zig build just for kicks

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("cracken", null);
+    exe.setBuildMode(mode);
+    
+    exe.addCSourceFiles(&[_][] const u8 {
+        "app.c",
+        "curve.c",
+        "deviceenumerator.c",
+        "hiddevice.c",
+        "kraken.c",
+        "krakenwidget.c",
+        "layout.c",
+        "list.c",
+        "main.c",
+        "mainwindow.c",
+        "window.c",
+        "xalloc.c"
+        },
+        &[_][] const u8 { "-DUNICODE" });
+    
+    exe.linkLibC();
+    exe.linkSystemLibrary("setupapi");
+    exe.linkSystemLibrary("hid");
+    exe.linkSystemLibrary("comctl32");
+    exe.linkSystemLibrary("gdi32");
+    exe.subsystem = .Windows;
+    
+    exe.install();
+}
