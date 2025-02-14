9e2d082257dd66a17158dbf721985c306b33ae32
Henri Vasserman <henv@hot.ee>
2025-02-14 13:43:47 +0200

    debug stuff

--- app.zig
+++ app.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const win32 = @import("win32.zig");
 
 pub var instance: win32.HINSTANCE = undefined;
 pub var allocator: std.mem.Allocator = undefined;
+pub const is_debug = builtin.mode == .Debug;
 
 comptime {
     @export(instance, .{ .name = "App_instance", .linkage = .strong });
