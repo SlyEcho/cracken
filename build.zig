a3e4c8a7d57d87cfd916c16f8b7c3d33f26cdc13
Henri Vasserman <henri.vasserman@bondora.com>
2025-06-17 15:18:22 +0300

    add check step

--- build.zig
+++ build.zig
@@ -9,11 +9,14 @@ pub fn build(b: *std.Build) !void {
         },
     });
 
-    const exe = b.addExecutable(.{
-        .name = "cracken",
+    const mod = b.addModule("cracken", .{
         .root_source_file = b.path("cracken.zig"),
         .target = tgt,
         .optimize = mode,
+    });
+    const exe = b.addExecutable(.{
+        .name = "cracken",
+        .root_module = mod,
         .win32_manifest = b.path("cracken.manifest"),
     });
 
@@ -43,4 +46,11 @@ pub fn build(b: *std.Build) !void {
     run_exe.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run cracken");
     run_step.dependOn(&run_exe.step);
+
+    const check_exe = b.addExecutable(.{
+        .name = "cracken_check",
+        .root_module = mod,
+    });
+    const check_step = b.step("check", "Check compiler");
+    check_step.dependOn(&check_exe.step);
 }
